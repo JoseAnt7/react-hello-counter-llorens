b55d126fa11f232a3ae0b87a0d5e245dedb5aaec
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-10-24 20:38:53 +0000

    dejando, solo contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -18,18 +18,7 @@ const Home = ({number, number2, number3, number4, number5, number6}) => {
 				<Number_counter value={number2}/>
 				<Number_counter value={number}/>
 			</div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 };
--- src/js/component/numbers.jsx
+++ src/js/component/numbers.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const Number_counter = ({value}) =>{
-    return(
-    <div className="bg-dark text-white">
-        <div class="container mt-5">
-            <div className="d-flex justify-content-center align-items-center border border-2">
-                <h1 className="mb-0">{value}</h1>
+export const Number_counter = ({ value }) => {
+    return (
+        <div className="bg-dark text-white min-vh-100 d-flex justify-content-center align-items-center">
+            <div className="container">
+                <div className="d-flex justify-content-center align-items-center border border-light border-2 rounded-circle p-4">
+                    <h1 className="mb-0 display-1">{value}</h1>
+                </div>
             </div>
         </div>
-    </div>
     );
-}  
+}
